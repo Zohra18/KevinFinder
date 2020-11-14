0536ca7325c867ba3be09a384d2be2d655f94acc
Zohra18 <ach.zohra@gmail.com>
2020-11-14 12:57:53 +0100

    Initial Commit

---
+++ KevinFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KevinFinder
+//
+//  Created by Zohra Achour on 14/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KevinFinder/KevinFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  KevinFinderApp.swift
+//  KevinFinder
+//
+//  Created by Zohra Achour on 14/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct KevinFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
